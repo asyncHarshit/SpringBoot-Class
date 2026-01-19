026932c1998126dec4f16fc3e111d98c24e2f489
harshit <harshitkumarsingh203@gmail.com>
2026-01-19 12:49:38 +0530

    push

---
+++ Day1/Day1_Revison/src/main/java/com/example/day1_revison/Day1RevisonApplication.java
@@ -0,0 +1,13 @@
+package com.example.day1_revison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1RevisonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day1RevisonApplication.class, args);
+    }
+
+}
